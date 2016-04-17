e288063597a113c8d6d4e3186ddbcdcd8424666b
Mikulcik <simon_mikulcik@mymail.eku.edu>
2016-04-16 22:50:46 -0400

    added methods

--- index.js
+++ index.js
@@ -65,47 +65,91 @@ var adv_types = ['RB','RBR'];
 var verb_types = ['VB','VBD', 'VBN','VBP','VBZ']; // base, past, past part., present,
 
 var nouns = [];//["person", "man", "woman"];
-var adjs = ["awesome", "terrible", "cool"];
-var advs = ["slowly", "quickly", "superbly"];
-var verbs = ["went", "did", "slay"];
+var adjs = [];//["awesome", "terrible", "cool"];
+var advs = [];//["slowly", "quickly", "superbly"];
+var verbs = [];//["went", "did", "slay"];
 prompt.start();
 
 var tagger = new Tagger(lexicon_file, rules_file, default_category, function(error) {
 	prompt.get(['sentence'], function (err, result) {
-		var tagged = tagger.tag(result.sentence.split(" "));
-		console.log(tagged);
 		
-		for(var i=0;i<tagged.length;i++){
-			if(noun_types.indexOf(tagged[i][1]) > -1)
-				nouns.push(tagged[i][0]);
-			if(adj_types.indexOf(tagged[i][1]) > -1)
-				adjs.push(tagged[i][0]);
-			if(adv_types.indexOf(tagged[i][1]) > -1)
-				advs.push(tagged[i][0]);
-			if(verb_types.indexOf(tagged[i][1]) > -1)
-				verbs.push(tagged[i][0]);
-		}
+		addWords(result.sentence.split(" "));
 		
-		for(var i=0;i<nouns.length;i++){
-			wordnet.lookup(nouns[i], function(results) {
-				console.log(results[0]);
-				results.forEach(function(result){
-					result.synonyms.forEach(function(result) {
-						nouns.push(result);
-						console.log("SYN: " + result);
-					});
-				});
-			});
-		}
-		
-		console.log(sprintf(
-			"Once upon a time a %(n1)s %(adv)s %(v)s the %(adj)s %(n2)s", 
-			{
-				"n1":nouns[Math.floor(nouns.length*Math.random())],
-				"n2":nouns[Math.floor(nouns.length*Math.random())],
-				"adj":adjs[Math.floor(adjs.length*Math.random())],
-				"adv":advs[Math.floor(advs.length*Math.random())],
-				"v": verbs[Math.floor(verbs.length*Math.random())]
-			}));
+		getSynonyms().then(function(){
+			
+			console.log(sprintf(
+				"Once upon a time a %(n1)s %(adv)s %(v)s the %(adj)s %(n2)s", 
+				{
+					"n1":nouns[Math.floor(nouns.length*Math.random())],
+					"n2":nouns[Math.floor(nouns.length*Math.random())],
+					"adj":adjs[Math.floor(adjs.length*Math.random())],
+					"adv":advs[Math.floor(advs.length*Math.random())],
+					"v": verbs[Math.floor(verbs.length*Math.random())]
+				}));
+		});
 	});
+
+	var addWords = function(list){
+		var tagged = tagger.tag(list);
+			//console.log(tagged);
+			
+			for(var i=0;i<tagged.length;i++){
+				var word = tagged[i][0];
+				var type = tagged[1]
+				if(noun_types.indexOf(tagged[i][1]) > -1)
+					nouns.push(tagged[i][0]);
+				if(adj_types.indexOf(tagged[i][1]) > -1)
+					adjs.push(tagged[i][0]);
+				if(adv_types.indexOf(tagged[i][1]) > -1)
+					advs.push(tagged[i][0]);
+				if(verb_types.indexOf(tagged[i][1]) > -1)
+					verbs.push(tagged[i][0]);
+			}
+	};
+	var getSynonyms = function(){
+		var promises = [];
+		//noun
+		for(var i=0;i<nouns.length;i++){
+			promises.push(new Promise(function(resolve, reject){
+				wordnet.lookup(nouns[i], function(results) {
+					Promise.all(results.map(function(result){
+						addWords(result.synonyms);
+					})).then(function(){resolve()});
+				});
+			}));
+		}
+		//adjs
+		for(var i=0;i<adjs.length;i++){
+			promises.push(new Promise(function(resolve, reject){
+				wordnet.lookup(adjs[i], function(results) {
+					Promise.all(results.map(function(result){
+						addWords(result.synonyms);
+					})).then(function(){resolve()});
+				});
+			}));
+		}
+		//advs
+		for(var i=0;i<advs.length;i++){
+			promises.push(new Promise(function(resolve, reject){
+				wordnet.lookup(advs[i], function(results) {
+					Promise.all(results.map(function(result){
+						addWords(result.synonyms);
+					})).then(function(){resolve()});
+				});
+			}));
+		}
+		//verb
+		for(var i=0;i<verbs.length;i++){
+			promises.push(new Promise(function(resolve, reject){
+				wordnet.lookup(verbs[i], function(results) {
+					Promise.all(results.map(function(result){
+						addWords(result.synonyms);
+					})).then(function(){resolve()});
+				});
+			}));
+		}
+		
+		return Promise.all(promises);
+	};
 });
+
